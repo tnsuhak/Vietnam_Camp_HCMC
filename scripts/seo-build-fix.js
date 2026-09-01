ba4cd563fa853a460ad7bd62381ee9eb283b8955
tnsuhak <tnsuhak@gmail.com>
2026-09-01 16:17:37 +0900

    Use standalone HCMC Netlify SEO URLs

--- scripts/seo-build-fix.js
+++ scripts/seo-build-fix.js
@@ -1,7 +1,6 @@
 const fs = require('fs');
 
-const HCMC = 'https://tnsuhak.github.io/Vietnam_Camp_HCMC/';
-const NT = 'https://tnsuhak.github.io/Vietnam_Camp_NT/';
+const HCMC = 'https://vietnam-camp-hcmc.netlify.app/';
 
 function setSeoUrl(html, url) {
   html = html.replace(/<link rel="canonical" href="[^"]*">/, `<link rel="canonical" href="${url}">`);
@@ -9,17 +8,11 @@ function setSeoUrl(html, url) {
   return html;
 }
 
-let hcmc = fs.readFileSync('index.html', 'utf8');
-hcmc = setSeoUrl(hcmc, HCMC);
-fs.writeFileSync('index.html', hcmc);
-
-if (fs.existsSync('nhatrang.html')) {
-  let nt = fs.readFileSync('nhatrang.html', 'utf8');
-  nt = setSeoUrl(nt, NT);
-  fs.writeFileSync('nhatrang.html', nt);
-}
+let html = fs.readFileSync('index.html', 'utf8');
+html = setSeoUrl(html, HCMC);
+fs.writeFileSync('index.html', html);
 
 fs.writeFileSync('robots.txt', `User-agent: *\nAllow: /\n\nSitemap: ${HCMC}sitemap.xml\n`);
 fs.writeFileSync('sitemap.xml', `<?xml version="1.0" encoding="UTF-8"?>\n<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">\n  <url>\n    <loc>${HCMC}</loc>\n    <lastmod>2026-09-01</lastmod>\n  </url>\n</urlset>\n`);
 
-console.log('Verified HCMC canonical, robots.txt and sitemap.xml; Nha Trang preview canonical points to its own site.');
+console.log('Verified HCMC canonical, robots.txt and sitemap.xml for the standalone Netlify site.');
